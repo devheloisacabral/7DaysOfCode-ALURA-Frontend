c49435e11f5c445594d5a0d0926127be21101db6
Heloisa Cabral <heloisacabralco@gmail.com>
2023-12-16 12:34:28 -0300

    Day 1 e 2 - Criação do Formulario e Requisitos

---
+++ 7DaysOfCode-JS-DOM/script.js
@@ -0,0 +1,11 @@
+function lettersOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
+        ((evt.which) ? evt.which : 0));
+    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
+        (charCode < 97 || charCode > 122)) {
+        alert("O campo de nome só aceita letras");
+        return false;
+    }
+    return true;
+}
